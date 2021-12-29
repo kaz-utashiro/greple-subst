8ab429ff5e6ab69a59b3875a2d9389ccc71ca1b2
Kazumasa Utashiro <kaz@utashiro.com>
2021-12-29 13:53:20 +0900

    Update for new t/runner interface.

--- t/Util.pm
+++ t/Util.pm
@@ -4,7 +4,17 @@ use utf8;
 use open IO => ':utf8', ':std';
 
 use lib 't/runner';
-use Greple;
+use Runner qw(get_path);
+
+my $greple_path = get_path('greple', 'App::Greple') or die Dumper \%INC;
+
+sub greple {
+    Runner->new($greple_path, @_);
+}
+
+sub run {
+    greple(@_)->run;
+}
 
 sub subst {
     greple '-Msubst', @_;
