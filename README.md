6f4e2962895ccc9af9848b17cf5b36701a182d0d
Kazumasa Utashiro <kaz@utashiro.com>
2022-04-04 18:27:12 +0900

    Checking in changes prior to tagging of version 2.3002.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b95799c..2bebd44 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension App-Greple-subst
     
     {{$NEXT}}
     
    +2.3002 2022-04-04T09:27:00Z
    +
    +    - Update ms-amend.dict
    +
     2.3001 2022-03-24T03:06:49Z
     
         - Remove 'pairwise' because it seems to have a bug causing core dump.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ subst - Greple module for text search and substitution
 
 # VERSION
 
-Version 2.3001
+Version 2.3002
 
 # SYNOPSIS
 
--- lib/App/Greple/subst.pm
+++ lib/App/Greple/subst.pm
@@ -6,7 +6,7 @@ subst - Greple module for text search and substitution
 
 =head1 VERSION
 
-Version 2.3001
+Version 2.3002
 
 =head1 SYNOPSIS
 
@@ -348,7 +348,7 @@ it under the same terms as Perl itself.
 use v5.14;
 package App::Greple::subst;
 
-our $VERSION = '2.3001';
+our $VERSION = '2.3002';
 
 use warnings;
 use utf8;
