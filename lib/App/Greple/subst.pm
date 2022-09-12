286d03ba15d3e8932eb80e2b82ab6525a4f7bcc4
Kazumasa Utashiro <kaz@utashiro.com>
2022-09-12 19:04:10 +0900

    Checking in changes prior to tagging of version 2.31.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2bebd44..77233d3 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension App-Greple-subst
     
     {{$NEXT}}
     
    +2.31 2022-09-12T10:04:00Z
    +
    +    - File handling part has now spun off to Greple::update module.
    +
     2.3002 2022-04-04T09:27:00Z
     
         - Update ms-amend.dict

--- lib/App/Greple/subst.pm
+++ lib/App/Greple/subst.pm
@@ -6,7 +6,7 @@ subst - Greple module for text search and substitution
 
 =head1 VERSION
 
-Version 2.3002
+Version 2.31
 
 =head1 SYNOPSIS
 
@@ -370,7 +370,7 @@ it under the same terms as Perl itself.
 use v5.14;
 package App::Greple::subst;
 
-our $VERSION = '2.3002';
+our $VERSION = '2.31';
 
 use warnings;
 use utf8;
