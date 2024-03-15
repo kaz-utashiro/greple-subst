47f40e48f6f0c048f0c8901b45be74df3e082e77
Kazumasa Utashiro <kaz@utashiro.com>
2024-03-15 13:31:03 +0900

    update copyright year

--- README.md
+++ README.md
@@ -341,7 +341,7 @@ Kazumasa Utashiro
 
 # LICENSE
 
-Copyright 2017-2023 Kazumasa Utashiro.
+Copyright 2017-2024 Kazumasa Utashiro.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
--- lib/App/Greple/subst.pm
+++ lib/App/Greple/subst.pm
@@ -360,7 +360,7 @@ Kazumasa Utashiro
 
 =head1 LICENSE
 
-Copyright 2017-2023 Kazumasa Utashiro.
+Copyright 2017-2024 Kazumasa Utashiro.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
